e89e979fdf28c6a81590b499ceb6d3293984e9d8
ildar <zariil@yandex.ru>
2023-04-25 21:41:35 +0500

    homework #4

--- src/main/java/ru/gb/preparation/hw_4/task_1/query.sql
+++ src/main/java/ru/gb/preparation/hw_4/task_1/query.sql
@@ -1,13 +1,17 @@
+-- ошибки в расписании (фильмы накладываются друг на друга),
+-- отсортированные по возрастанию времени.
+-- Выводить надо колонки
+-- «фильм 1», «время начала», «длительность», «фильм 2», «время начала», «длительность»;
 with t as (select *, starttime + duration * '1 minute'::interval as finish
            from session
                     join movie m on session.movie_id = m.id)
 
-select m1.title,
-       m1.startTime,
-       m1.duration,
-       m2.title,
-       m2.startTime,
-       m2.duration
+select m1.title     as "фильм 1",
+       m1.startTime as "время начала",
+       m1.duration  as "длительность",
+       m2.title     as "фильм 2",
+       m2.startTime as "время начала",
+       m2.duration  as "длительность"
 
 from t m1
          join t m2 on m1.session_id <> m2.session_id
--- src/main/java/ru/gb/preparation/hw_4/task_2/query.sql
+++ src/main/java/ru/gb/preparation/hw_4/task_2/query.sql
@@ -1,3 +1,7 @@
+-- перерывы 30 минут и более между фильмами —
+-- выводить по уменьшению длительности перерыва. Колонки:
+-- «фильм 1», «время начала», «длительность», «время начала второго фильма», «длительность перерыва»;
+
 with t as (select *,
                   starttime + duration * '1 minute'::interval   as finish,
                   lead(starttime) over (order by starttime)     as next_start,
@@ -5,11 +9,11 @@ with t as (select *,
            from session
                     join movie m on session.movie_id = m.id)
 
-select title,
-       starttime,
-       duration,
-       next_start,
-       extract(epoch from next_start - movies_end) / 60 as break
+select title as "фильм 1",
+       starttime as "время начала",
+       duration as "длительность",
+       next_start as "время начала второго фильма",
+       extract(epoch from next_start - movies_end) / 60 as "длительность перерыва"
 from t
 where next_start - movies_end >= '30 minutes'::interval
 
